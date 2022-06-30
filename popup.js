8fecc5160cd5aac70bee5c87567a92c5e4b034f4
Devpoa Software <suporte.devpoa@gmail.com>
2022-06-30 02:40:28 -0300

    Ajuste nos avisos e retorno de dados em json

--- popup.js
+++ popup.js
@@ -6,8 +6,9 @@ let user         = document.querySelector('#usuario');
 let senha        = document.querySelector('#senha');
 let form         = document.querySelector('#meuform');
 let avisoCampos  = document.querySelector("#ext-aviso-campo");
+let avisoUsuario  = document.querySelector("#ext-aviso-usuario");
 let avisoSucesso  = document.querySelector("#ext-aviso");
-let checkbox      = document.querySelector("input[name=chkAtivo]");
+let checkbox       = document.querySelector("input[name=chkAtivo]");
 let aviso = document.querySelector(".aviso");
 const ativarExtensao = document.querySelector('#btnAtivar');
 
@@ -20,7 +21,7 @@ ativarExtensao.addEventListener('click', function(e){
           avisoCampos.removeAttribute('hidden', 'hidden');
           checkbox.checked = false;
           setTimeout(showForm, 3000);
-          aviso.innerHTML = "Extensão está desativada";
+          aviso.innerHTML = "Extensï¿½o estï¿½ desativada";
           return false;
      } else {
 
@@ -29,7 +30,7 @@ ativarExtensao.addEventListener('click', function(e){
                cdsenha : senha.value
           }
 
-          fetch(`http://localhost/8.20/getAuthExtension.php?nmusuariorede=${data.nmusuariorede}&cdsenha=${data.cdsenha}`, {
+          fetch(`http://localhost/login/login.php?usuario=${data.nmusuariorede}&senha=${data.cdsenha}`, {
                headers: {
                Accept: "application/json",
                "Content-Type": "application/json",
@@ -38,21 +39,28 @@ ativarExtensao.addEventListener('click', function(e){
             body: JSON.stringify(data)
           }).then((r) => r.json())
             .then(r => {
-                    if(r.ok){
+                    if(r.nome == data.nmusuariorede){
                          form.setAttribute('hidden', 'hidden');
                          avisoSucesso.removeAttribute('hidden', 'hidden');
                          setTimeout(showForm, 3000);
                          checkbox.checked = true;
-                         aviso.innerHTML = "Extensão está ativada";
+                         aviso.innerHTML = "Extensï¿½o estï¿½ ativada";
                          setTimeout(hiddenSucess, 3000);
+                         clearInput();
+                    } else {
+                         form.setAttribute('hidden', 'hidden');
+                         avisoUsuario.removeAttribute('hidden', 'hidden');
+                         avisoUsuario.innerHTML = r.error_message;
+                         setTimeout(showForm, 3000);
+                         setTimeout(hiddenError, 3000);
                     }
-                    console.log(r)
                }).catch((error) => {
                     console.log(error)
           });
      }
          
 });
+
 
 const verificaCampos = function(){ 
      if(user.value == "" && senha.value == ""){
@@ -61,11 +69,10 @@ const verificaCampos = function(){
      return true;
 }
 
-const hideLoading = function(){
-     icon.classList.add('hidden-icon');
-     icon.classList.remove('icon');
+const clearInput = function(){
+     user.value = "";
+     senha.value = "";
 }
-
 
 const showForm = function() {
      form.removeAttribute('hidden', 'hidden');
@@ -75,73 +82,6 @@ const showForm = function() {
 const hiddenSucess = function (){
      avisoSucesso.setAttribute('hidden', 'hidden');
 } 
-
-url = window.location.href + '';
-
-console.log(url)
-
-/*$(document).ready(function(){
-     $('#btnAtivar').on("click", function() {
-          var user = $("#usuario").val();
-          var pass = $("#senha").val();
-
-          //alert(user + " " + pass);
-          if(user == "" || pass == ""){
-               $('#btnAtivar').removeClass('btn-primary');
-               $('#btnAtivar').addClass('btn-danger');
-               $('#btnAtivar').attr('disabled', 'disabled');
-               $('#btnAtivar').text('Erro ao ativar');      
-               $('#ext-aviso').show(); 
-               $('#ext-aviso').html('ExtensÃ£o nÃ£o ativada'); 
-               $('#ext-aviso').removeClass('alert-success');
-               $('#ext-aviso').addClass('alert-danger');
-               setTimeout(returnToDefault, 3000);
-          } 
-          $.ajax({
-              
-               url: 'http://localhost/8.20/framework/login/request/atendente/getLogin.php',
-               type: 'POST',
-               data :{
-                    cdusuario : user,
-                    cdsenha : pass
-               },
-               dataType: 'text',
-               success: function(response){
-                    console.log(response);
-                    /*if(response == "Extensão ativada"){
-                         browser.storage.local.get(null, res => {
-                              //let rgxstr = (res.regstr_allowed || regexPadrao);
-                              let lista = document.querySelector('#lista');
-                              for(var i = 0; i<regexPadrao.length; i++){
-                                   let tagLi = document.createElement('li');
-                                   tagLi.appendChild(document.createTextNode(regexPadrao[i]));
-                                   lista.appendChild(tagLi);
-                              }
-                              document.querySelector("#chkAtivo").checked = res.is_disabled;
-                         });
-                         
-                         
-                         window.onload = function(){
-                              let dsv_checkbox = $('#chkAtivo');
-                         
-                              dsv_checkbox.change(function(){
-                                   port.postMessage({
-                                        is_disabled : dsv_checkbox.checked
-                                   });
-                              });
-                         }
-                    }*/
-              // }
-          //})
-     //});*/
-//});
-
-function returnToDefault(){
-     $('#btnAtivar').addClass('btn-primary');
-     $('#btnAtivar').removeClass('btn-danger');
-     $('#btnAtivar').removeAttr('disabled'); 
-     $('#btnAtivar').text('Ativar Extensão');    
-     $('#ext-aviso').hide(); 
-     $('#ext-avis').hide(); 
-     
+const hiddenError = function (){
+     avisoUsuario.setAttribute('hidden', 'hidden');
 }
